7f04b5b860ff435ea7198173123d2c0b584d8c43
Bambosh <Sebastianmbsmith@gmail.com>
2019-09-29 19:35:12 +1000

    Initial commit

--- add_data.php
+++ add_data.php
@@ -15,19 +15,24 @@ $no_sold   = mysqli_escape_string($dbConnection, $_POST["no_sold"]);
 $date_sold = mysqli_escape_string($dbConnection, $_POST["date_sold"]);
 $discount  = mysqli_escape_string($dbConnection, $_POST["discount"]);
 
+$dbConnection = connectToDb();
 
-
-if (mysqli_query($dbConnection, "SELECT * FROM ITEMS WHERE Item_ID = $item_id"))
-{
-    mysqli_query($dbConnection, "INSERT INTO SALES (Item_ID, Quantity_Sold, Date_Sold, Discount)
-                                 VALUES ($item_id, $no_sold, $date_sold, $discount);");
+if($dbConnection){
+    if (mysqli_query($dbConnection, "SELECT * FROM ITEMS WHERE Item_ID = $item_id"))
+    {
+        mysqli_query($dbConnection, "INSERT INTO SALES (Item_ID, Quantity_Sold, Date_Sold, Discount)
+                                     VALUES ($item_id, $no_sold, $date_sold, $discount);");
+    }
+    else
+    {
+        echo "Item does not exist";
+    }
 }
-else
-{
-    echo "Item does not exist";
+else {
+    //echo "<p>Database connection failure"</p>
 }
 
-header ('location: viewTable.html');
+header ('location: viewTable.php');
 exit();
  
 ?>
