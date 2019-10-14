fbbeb089aae32f877844c3bfa3454018b4d58a1d
101629257 <101629257@student.swin.edu.au>
2019-10-14 22:58:41 +1100

    Sales sorting
    Changed display to sort for weekly total and monthly sales

--- php_scripts/displaySales.php
+++ php_scripts/displaySales.php
@@ -1,6 +1,19 @@
 <?php
 	include 'php_scripts/db_connection.php';
 
+function showColumns($row_sale)
+{
+	echo '<tr>';
+	echo '<td>' . $row_sale['Sale_ID'] . '</td>';
+	echo '<td>' . $row_sale['Item_ID'] .'</td>';
+	echo '<td>' . $row_sale['Product_Name'] .'</td>';
+	echo '<td>$' . $row_sale['Price'] .'</td>';
+	echo '<td>' . $row_sale['Quantity_Sold'] .'</td>';
+	echo '<td>' . $row_sale['Date_Sold'] .'</td>';
+	echo '<td>' . $row_sale['Discount'] . '% </td> </tr>';
+	
+}
+
 function getColumns()
 {
 
@@ -15,10 +28,13 @@ function getColumns()
         <td> N/A </td>
         <td> N/A </td>
     </tr>';
+	
 
-    if(!$db_connection){
+    if(!$db_connection)
+	{
     }
-    else {
+    else 
+	{
         $sale_query = mysqli_query($db_connection, "SELECT sales.Sale_ID, sales.Item_ID, items.Product_Name, sales.Quantity_Sold, sales.Date_Sold, items.Price, sales.Discount 
         FROM sales, items 
         WHERE items.Item_ID = sales.Item_ID
@@ -29,18 +45,53 @@ function getColumns()
         {
             echo $failed_row;
         }
-        else{
-            while ($row_sale = mysqli_fetch_assoc($sale_query)){
-                
-                echo '<tr>';
-                echo '<td>' . $row_sale['Sale_ID'] . '</td>';
-                echo '<td>' . $row_sale['Item_ID'] .'</td>';
-                echo '<td>' . $row_sale['Product_Name'] .'</td>';
-                echo '<td>$' . $row_sale['Price'] .'</td>';
-                echo '<td>' . $row_sale['Quantity_Sold'] .'</td>';
-                echo '<td>' . $row_sale['Date_Sold'] .'</td>';
-                echo '<td>' . $row_sale['Discount'] . '% </td> </tr>';
-            }
+        else
+		{
+			if((!isset($_COOKIE['View'])) || ($_COOKIE['View'] == 'Total'))
+			{
+				while ($row_sale = mysqli_fetch_assoc($sale_query))
+				{				
+					showColumns($row_sale);		
+				}
+			}
+			else
+			{
+				
+				if($_COOKIE['View'] == 'Monthly')
+				{
+					$year = date('Y');
+					$month = date('m');
+					
+					while ($row_sale = mysqli_fetch_assoc($sale_query))
+					{				
+						$current = explode("-",$row_sale['Date_Sold']);
+							
+						if (($current[1] == $month) && ($current[0] == $year))
+						{
+							showColumns($row_sale);
+						}			
+					}
+				}
+				
+				if($_COOKIE['View'] == 'Weekly')
+				{
+					$FirstDay = date("Y-m-d", strtotime('sunday last week'));  
+					$LastDay = date("Y-m-d", strtotime('sunday this week'));  
+					
+					while ($row_sale = mysqli_fetch_assoc($sale_query))
+					{				
+						$Date = ($row_sale['Date_Sold']);
+						if ($Date > $FirstDay && $Date < $LastDay)
+						{
+							showColumns($row_sale);
+						}			
+					}
+				}
+				
+			}
+			
+			
+			
         }
     }
 }
